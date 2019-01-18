deaee2fe0ab6fb9d8fcd9969f09a2923a13a9920
Пентюгов Юрий Владимирович <YuPentyugov@nvg.ru>
2019-01-18 11:06:37 +0300

    Add Task11

---
+++ src/test/Test11.java
@@ -0,0 +1,24 @@
+//Сделайте программу, которая будет проверять является ли число типа double целым
+package test;
+
+import java.util.Scanner;
+
+public class Test11 {
+
+    public static void isNumberC() {
+        System.out.println("Введите число типа double");
+        Scanner sc = new Scanner(System.in);
+        double d = sc.nextDouble();
+        if (d == 0) {
+            System.out.println("Это число ноль!");
+        } else if (d % 2 == 0) {
+            System.out.println("Это целое число!");
+        } else {
+            System.out.println("Это не целое число!");
+        }
+    }
+
+    public static void main(String[] args) {
+        isNumberC();
+    }
+}
